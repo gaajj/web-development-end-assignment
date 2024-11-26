d35af69e7b0577b46c6b49210e8eb8a4b14a2d57
gaxzy <daantas224@gmail.com>
2024-11-26 23:16:21 +0100

    Update card.php

--- app/public/components/card.php
+++ app/public/components/card.php
@@ -1,10 +1,14 @@
 <?php
-function renderCard($title, $description) {
+function renderCard($title, $description, $upvote, $downvote) {
     ?>
     <div class="card shadow-sm" style="width: 18rem;">
         <div class="card-body">
             <h5 class="card-title"><?php echo htmlspecialchars($title, ENT_QUOTES, 'UTF-8'); ?></h5>
             <p class="card-text"><?php echo htmlspecialchars($description, ENT_QUOTES, 'UTF-8'); ?></p>
+            <div class="d-flex justify-content-center align-items-center">
+                <span class="text-success mx-2"><?php echo htmlspecialchars($upvote, ENT_QUOTES, 'UTF-8'); ?></span>
+                <span class="text-danger mx-2"><?php echo htmlspecialchars($downvote, ENT_QUOTES, 'UTF-8'); ?></span>
+            </div>
         </div>
     </div>
     <?php
