cbcd38c1ee2c0cb00626fc7687f61f7e1688fd44
gaxzy <daantas224@gmail.com>
2024-11-26 23:27:41 +0100

    Swap logout and welcome message

--- app/public/component/navbar.php
+++ app/public/component/navbar.php
@@ -16,8 +16,8 @@
             </ul>
             <div class="d-flex">
                 <?php if (!empty($_SESSION['user_id'])): ?>
+                    <span class="navbar-text me-2">Hi, <?php echo htmlspecialchars($_SESSION['username'], ENT_QUOTES, 'UTF-8'); ?></span>
                     <a href="/logout.php" class="btn btn-outline-secondary me-2">Logout</a>
-                    <span class="navbar-text">Hi, <?php echo htmlspecialchars($_SESSION['username'], ENT_QUOTES, 'UTF-8'); ?></span>
                 <?php else: ?>
                     <a href="/login.php" class="btn btn-outline-primary me-2">Login</a>
                     <a href="/register.php" class="btn btn-primary">Register</a>
